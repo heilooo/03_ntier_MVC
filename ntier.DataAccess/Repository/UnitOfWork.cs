38fcf80547c6eb0259d20afb9431cb65b3e0f4f3
abbas dadvand <70462416+heilooo@users.noreply.github.com>
2024-02-12 14:15:25 +0330

    Add project files.

---
+++ ntier.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using ntier.DataAccess.Repository.IRepository;
+using ntire.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ntier.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category=new CategoryRepository(_db);
+        }
+        public ICategoryRepository Category { get; private set; }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
